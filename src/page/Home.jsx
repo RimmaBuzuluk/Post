87ad5e3b0f6ffca7bd11c7075e49d51346191af4
RimmaBuzuluk <rimmysya2000@gmail.com>
2023-08-23 20:47:39 +0300

    use Despatch

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import axios from '../axios';
+import {useDispatch} from "react-redux"
+import { fetchPosts } from '../redux/slices/post';
 
 
 export const Home = () => {
+
+  const dispatch=useDispatch()
   React.useEffect(()=>{
-    axios.get('/posts')
+    // axios.get('/posts')
+    dispatch(fetchPosts())
   },[])
 
   return (
