5f2321464a3fd6ddbc863fd85d53bece9b5b2e1b
RimmaBuzuluk <rimmysya2000@gmail.com>
2023-08-26 21:24:19 +0300

    page +-(css)

--- src/component/Page/Page.jsx
+++ src/component/Page/Page.jsx
@@ -1,24 +1,73 @@
 import React from 'react';
-import { Typography, Paper } from '@mui/material';
+import { Typography, Paper, Grid, Avatar, IconButton, Chip } from '@mui/material';
+import { FavoriteBorderOutlined, ModeCommentOutlined } from '@mui/icons-material';
 
-const Page = ({title}) => {
+const Page = ({ id, title, text,avatarUrl, imageUrl, user, createdAt, viewsCount, commentsCount, tags, fixedSize }) => {
 
+  
   const styles = {
     paper: {
       padding: 20,
-      margin: '20px', // Додавання відступу 10px
-      height: '600px'
-    }
+      margin: '50px',
+      width: '1200px',
+      height: '1000px'
+    },
+    image: {
+      width: '100%',
+      height: 'auto',
+    },
+    authorInfo: {
+      display: 'flex',
+      alignItems: 'center',
+      marginTop: 10,
+    },
+    authorAvatar: {
+      marginRight: 10,
+    },
+    tagContainer: {
+      marginTop: 10,
+    },
+    tag: {
+      marginRight: 5,
+    },
+    likeCommentContainer: {
+      display: 'flex',
+      alignItems: 'center',
+      marginTop: 10,
+    },
+    likeCommentIcon: {
+      marginRight: 5,
+    },
   };
 
   return (
     <Paper elevation={3} style={styles.paper}>
+      <img src={imageUrl} alt={title} style={styles.image} />
+      <div style={styles.authorInfo}>
+        {/* <Avatar src={user.avatarUrl} style={styles.authorAvatar} /> */}
+        <div>
+          {/* <Typography variant="subtitle1">user.fullName</Typography>
+          <Typography variant="caption">{createdAt}</Typography> */}
+        </div>
+      </div>
       <Typography variant="h4" gutterBottom>
         {title}
       </Typography>
-      <Typography variant="body1">
-        Основний текст вашої статті тут. Можливо, ви захочете використовувати багато різних компонентів Material-UI для створення контенту статті.
-      </Typography>
+      <div style={styles.tagContainer}>
+        {/* {tags.map((tag) => (
+          <Chip key={tag} label={tag} style={styles.tag} />
+        ))} */}
+      </div>
+      <div style={styles.likeCommentContainer}>
+        <IconButton size="small" style={styles.likeCommentIcon}>
+          <FavoriteBorderOutlined />
+        </IconButton>
+        <Typography variant="caption">{viewsCount}</Typography>
+        <IconButton size="small" style={styles.likeCommentIcon}>
+          <ModeCommentOutlined />
+        </IconButton>
+        <Typography variant="caption">{commentsCount}</Typography>
+      </div>
     </Paper>
   );
 };
--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -31,19 +31,16 @@ export const Home = () => {
         <Grid xs={8} item>
           {(isPostsLoading ? [...Array(5)]: posts.item).map((obj, index) => 
           isPostsLoading ? (
-            <Page key={index} isLoading={true}/>
+            <Page key={index} isLoading={true} fixedSize={true}/>
             
           ):(
             <Page 
             id={obj._id}
             title={obj.title}
+            text={obj.text}
             imageUrl="https://res.cloudinary.com/practicaldev/image/fetch/s--UnAfrEG8--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/icohm5g0axh9wjmu4oc3.png"
-            user={{
-              avatarUrl:
-                'https://res.cloudinary.com/practicaldev/image/fetch/s--uigxYVRB--/c_fill,f_auto,fl_progressive,h_50,q_auto,w_50/https://dev-to-uploads.s3.amazonaws.com/uploads/user/profile_image/187971/a5359a24-b652-46be-8898-2c5df32aa6e0.png',
-              fullName: 'Keff',
-            }}
-            createdAt={'12 июня 2022 г.'}
+            user={obj.user}
+            createdAt={obj.createdAt}
             viewsCount={150}
             commentsCount={3}
             tags={['react', 'fun', 'typescript']}
