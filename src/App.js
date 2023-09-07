589f3c09fe01f7a38471a4489c68665ef4725c2d
RimmaBuzuluk <rimmysya2000@gmail.com>
2023-09-07 18:38:52 +0300

    css addPost, registration, login

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './Header';
 import { Home } from './page/Home';
+import { FullPost } from './page/FullPost';
+import { Login } from './page/Login';
+import { Registration } from './page/Registration';
+import { AddPost } from './page/AddPost';
 
 function App() {
   return (
@@ -11,11 +15,10 @@ function App() {
       <div className='MainInformation'>
         <Routes>
           <Route path='/' element={<Home />}/>
-        
-        {/*<FullPost />*/}
-        {/*<AddPost />*/}
-        {/*<Login />*/}
-        {/*<Registration />*/}
+          <Route path='/posts:id' element={<FullPost />} />
+          <Route path='/add-post' element={<AddPost />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Registration />} />
         </Routes>
         </div>
     </div>
